69746d28ef256bc63cc313e4624e24fad82d7c43
Pablo <contreraspablo197@protonmail.com>
2021-11-06 14:02:50 -0600

    v1.0.0_multiSlaveSupport_variablesToArrays

--- maestromulticanal.ino
+++ maestromulticanal.ino
@@ -2,27 +2,39 @@
 
 
 //configuracion 
-    int voltageMaxSet = 19653; //tope maximo de voltaje para el seteo de la resistencia
-    unsigned int voltageMinSet = 6551; //tope minimo para el seteo de la resistencia
-    unsigned int measureVoltage  = 1310; //voltaje que se aplica para medir la resistencia 
-
-    unsigned int vMax = 32767+voltageMaxSet;
-    unsigned int vMin = 32767+voltageMinSet;
-    unsigned int vMed = 32767+measureVoltage; 
+    const int slavesQty = 1; //define el numero de esclavos a utilizar 
     
-    unsigned int delta = 1;
+    int voltageMaxSet[slavesQty] = { //tope maximo de voltaje para el seteo de la resistencia
+      19653
+    };
+    unsigned int voltageMinSet[slavesQty] = { //tope minimo para el seteo de la resistencia
+      6551
+    };
+    unsigned int measureVoltage[slavesQty] = { //voltaje que se aplica para medir la resistencia 
+      1310
+    };
+    unsigned int electricCurrents[slavesQty];
+    
+    unsigned int delta[slavesQty] = {
+      1
+    };
+    int executeFlag[slavesQty] = { //una bandera por cada esclavo 
+      0
+    };
     
     
     
 // variables del programa: 
-    
-    
+    int stateMachine = 0; 
     
 //Declaracion de PINES: 
     //Puerto del Arduino: 
 
-    //Puerto SPI 1: 
-    int _slave1SS_ = 2;
+    //Puertos SPI: 
+    int _slaveSelect_[slavesQty] = {
+      2
+    };
+    
     //PINES
     int _arduinoClk_ = 7; 
     
@@ -47,6 +59,23 @@
       digitalWrite(slaveNumber,HIGH); //desactivar esclavo
     
     }
+    //Asignar variables del arreglo
+    void dataChange (){
+      for(int x; x<slavesQty; x++){
+          SPItransfer(_slaveSelect_[x], delta[x], 1); 
+          SPItransfer(_slaveSelect_[x], voltageMinSet[x], 2);
+          SPItransfer(_slaveSelect_[x], voltageMaxSet[x], 3);
+          SPItransfer(_slaveSelect_[x], measureVoltage[x], 4);
+      }
+    }
+    
+    void offsetValues(int offset) { // 32767
+      for(int x; x<slavesQty; x++){
+          voltageMinSet[x] += offset;
+          voltageMaxSet[x] += offset;
+          measureVoltage[x] += offset;
+      }
+    }
 
 void setup(){
   //inicializar pines Arduino: 
@@ -55,27 +84,49 @@ void setup(){
   //inicializar monitor serial: 
   Serial.begin(9600);
   
-  //Inicializar protocolo SPI: 
-  pinMode(_slave1SS_, OUTPUT);
+  //Inicializar protocolo SPI en cada esclavo:
+  for (int x; x<slavesQty; x++) {
+    pinMode(_slaveSelect_[x], OUTPUT);
+    digitalWrite(_slaveSelect_[x], HIGH);
+  } 
+
+  //Inicializar la conexion serial en el arduino: 
   SPI.begin();
-  digitalWrite(_slave1SS_, HIGH);           
   SPI.setClockDivider(SPI_CLOCK_DIV128);
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
   delay(2000);
-  SPItransfer(_slave1SS_ , delta, 1); 
-  SPItransfer(_slave1SS_, vMin, 2);
-  SPItransfer(_slave1SS_, vMax, 3);
-  SPItransfer(_slave1SS_, vMed, 4);
+  
+  offsetValues(32767);
+  dataChange();
+  
   Serial.println("ESTADOS ESCRITOS EN ATMEGA");
   
 }
 
+
+
 void loop() {
+  //controlar los pasos de la maquina de estados:
+  if(stateMachine < 5){ 
+    stateMachine++;
+  }else{stateMachine=1;}
+  
+  for (int x; x<slavesQty; x++){
+    if (executeFlag[x] != 0){ //si detecta que se necesita cambiar los valores de un esclavo: 
+      SPItransfer(x, delta[x]*(-1), 1); //cambiando el status de set a reset y de reset a set 
+    }
+  }
+  
+  if (stateMachine == 1){ 
+    
+  }
+  
+  
   
   digitalWrite(_arduinoClk_,HIGH);
-  delayMicroseconds(22);
+  delayMicroseconds(30);
   digitalWrite(_arduinoClk_,LOW);
-  delayMicroseconds(22);
+  delayMicroseconds(30);
   
 }
